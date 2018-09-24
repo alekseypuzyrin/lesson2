d5b1adee2dd0f96b4f1617dbf219fcf0bb303cb7
alekseypuzyrin <puzyrin7@mail.ru>
2018-09-24 20:04:30 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<!--boolean-->
+<!--true==1;//true-->
+<!--false==0//false-->
+<!--""==false-->
+<!--" "==true-->
+
+<!--oject-->
+<!--$obj=new a;-->
+<!--$obj->color='red';-->
+<!--echo $obj->color;-->
+
+<!--isser, unset//удаление переменной-->
+<!--$var=[];-->
+<!--if(isset($var)){//isser усть ли переменная-->
+<!--    echo "ok";-->
+<!--} else{-->
+<!--    echo "not";-->
+<!--}-->
+<?php
+//$var=12; //gettype узнать тип данных
+//echo gettype($var);
+
+?>
