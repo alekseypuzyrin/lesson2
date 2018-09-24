e6c431ee041a2500ba823681212383dcf3250699
alekseypuzyrin <puzyrin7@mail.ru>
2018-09-24 21:00:08 +0300

    first commit

--- index.php
+++ index.php
@@ -19,5 +19,40 @@
 <?php
 //$var=12; //gettype узнать тип данных
 //echo gettype($var);
+//$var =["or","gre"];
+//echo "<pre>";//аккуратный вывод массива
+//    print_r($var);
+//echo "</pre>";
+
+//комбинированные операторы
+//$n = 6;
+//$n += 1; // 1 прибавить к $n
+//echo "$n"
+//$message = "Hello";
+//$message .= "world!"; // Hello world!
+//echo "$message";
+//?>
+<!--//zadanie1-->
+<?php
+ $elem1=10;
+ $elem2=2;
+     echo "$elem1"+"$elem2";
+     echo "<br>";
+     echo "$elem1"-"$elem2";
+     echo "<br>";
+     echo "$elem1"*"$elem2";
+     echo "<br>";
+     echo "$elem1"/"$elem2";
+     echo "<br>";
+?>
+<!--zadanie2-->
+
+<?php
+    $a=25;
+    $b=70;
+    $c=100;
+    if($a>26 && $a<$b || $a=25 && $c==100 && $b==70 ){
+        echo "OK";
+    }
 
 ?>
